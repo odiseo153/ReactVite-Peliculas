b2a79157627dfe2f8035821d0879311d3c92fee7
odiseo153 <odiseorincon@gmail.com>
2023-05-13 18:41:56 -0400

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,39 @@ import './Components/styles/slider.css'
 import Pelis from './Components/Pelis'
 
 function App() {
+const[va,setVa] = useState(false)
 
+const mostrar = ()=>
+{
+let div = document.getElementById('yo')
+
+  
+
+  if(!va)
+  {
+  div.style.visibility = 'hidden'
+setVa(true)
+
+  }
+else
+{
+div.style.visibility = 'visible'
+setVa(false)
+}
+
+
+}
 
   return (
     <div className="App">
      
 
  <Pelis/>
+<button onClick={mostrar}>mostrar</button>
 
+<div id='yo'>
+<Presentacion />
+</div>
     </div>
   )
 }
--- src/Components/Pelis.jsx
+++ src/Components/Pelis.jsx
@@ -35,7 +35,7 @@ function Pelis() {
 
             )
           })}
-<Presentacion />
+
       </div>
 
     </div>
